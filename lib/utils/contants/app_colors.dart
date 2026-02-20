630398ac00d3c2cff4f5028e9f7aa26a6821b02a
Abdullah Khan Kakar <2022bbit71@student.uet.edu.pk>
2026-02-20 14:33:59 +0500

    add assets images and illustrations + download fonts + add common colors

--- lib/utils/contants/app_colors.dart
+++ lib/utils/contants/app_colors.dart
@@ -1,1 +1,16 @@
-class AppColors {}
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff258BA1);
+  static const Color red = Color(0xffED1F3E);
+  static const Color green = Color(0xff32B359);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff191D23);
+
+  // dark theme colors
+  static const Color textDark = Color(0xffC6C9CD);
+  static const Color darkBg = Color(0xff36393E);
+
+  // light theme colors
+  static const Color textLight = Color(0xff4B5768);
+}
